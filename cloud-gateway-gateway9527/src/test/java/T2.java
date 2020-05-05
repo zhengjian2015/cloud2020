d816dcec2a192c2718a611206793764686a6a057
zhengj <570470242@qq.com>
2020-05-05 22:56:33 +0800

    添加自定义过滤器

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,9 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+        //2020-05-05T22:08:31.219+08:00[Asia/Shanghai]
+    }
+}
