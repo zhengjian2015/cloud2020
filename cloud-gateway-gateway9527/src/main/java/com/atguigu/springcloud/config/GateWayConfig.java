f053e88d9c25c8afb3c7e4405442906461a9ca21
zhengj <570470242@qq.com>
2020-05-05 21:59:00 +0800

    配置动态路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 其实这个和yml里的routes的配置是一样的
+ *
+ */
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_atguigu",
+        r -> r.path("/guonei")
+                .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+}
