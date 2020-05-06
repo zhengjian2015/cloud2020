9b079081f35c28134cf70aea5ee3a8bccd321f51
zhengj <570470242@qq.com>
2020-05-06 22:02:56 +0800

    新增config配置

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
@@ -1,4 +1,4 @@
-package com.atguigu.springcloud.filter;
+ package com.atguigu.springcloud.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
