68c745daabc09759a6b4f83da627af3fe0107c90
zhengj <570470242@qq.com>
2020-05-27 23:58:12 +0800

    seata order

---
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/SeataOrderMainApp2001.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud.alibaba;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataOrderMainApp2001 {
+    public static void main(String[] args) {
+        SpringApplication.run(SeataOrderMainApp2001.class, args);
+    }
+}
---
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/service/StorageService.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.alibaba.service;
+
+import com.atguigu.springcloud.alibaba.domain.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * @auther zzyy
+ * @create 2020-02-26 15:22
+ *
+ * @FeignClient 是微服务简互相 再注册中心的
+ */
+@FeignClient(value = "seata-storage-service")
+public interface StorageService {
+    @PostMapping(value = "/storage/decrease")
+    CommonResult decrease(@RequestParam("productId") Long productId, @RequestParam("count") Integer count);
+}
